3808452ffc055715b0eab64ca1c45596b93de81e
Ali Bahari <alibahari007@gmail.com>
2022-09-08 19:37:41 +0430

    Add concurrent version of srtNameFixer with a small change in file name

--- srtNameFixer.sh
+++ srtNameFixer.sh
@@ -4,12 +4,11 @@
 # 1. Find the srt file of each mp4 file (In my case, srt filename starts with the filename of its corresponding mp4)
 # 2. Change the name of both the srt and mp4 file to some similar name (Numbers starting from 1 are used for the name)
 
-index=1
 for mp4File in *.mp4
 do
 	srtFile="${mp4File//.mp4/}"*.srt
-	mv "$mp4File" $index.mp4 | mv $srtFile $index.srt
-	let "index+=1"
+	mv "$mp4File" ${srtFile:0:2}.mp4 &
+	mv $srtFile ${srtFile:0:2}.srt &
 done
 
 # Changing the srt filename to the mp4 filename did not do the trick for me!
